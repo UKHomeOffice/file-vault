6a5fe3a5b4dfaf24118f27367189e3bf5b652947
Ben Marvell <madmarv187@gmail.com>
2017-01-30 15:41:59 +0000

    Log errors as they happen

--- controllers/file.js
+++ controllers/file.js
@@ -23,6 +23,12 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
+function logError(req, err) {
+  if (req.logger && req.logger.error) {
+    req.logger.error(err);
+  }
+}
+
 function deleteFileOnFinishedRequest(req, res, next) {
   if (req.file) {
     onFinished(res, () => {
@@ -47,7 +53,7 @@ function clamAV(req, res, next) {
     formData: fileData
   }, (err, httpResponse, body) => {
     if (err) {
-      req.logger.error(err);
+      logError(req, err);
       err = {
         code: 'VirusScanFailed'
       };
@@ -72,7 +78,7 @@ function s3Upload(req, res, next) {
 
   s3.putObject(params, (err) => {
     if (err) {
-      req.logger.error(err);
+      logError(req, err);
       err = {
         code: 'S3PUTFailed'
       };
@@ -92,7 +98,7 @@ router.get('/:id', (req, res) => {
     Bucket: config.get('aws.bucket'),
     Key: req.params.id
   }).createReadStream().on('error', (err) => {
-    req.logger.error(err);
+    logError(req, err);
     if (err.statusCode === 404) {
       res.status(404).end();
     }
